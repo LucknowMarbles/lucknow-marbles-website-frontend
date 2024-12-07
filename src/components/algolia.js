f1d928693c210ee83e50c5e662919a038d2c3945
Arpit Srivastava <srivastavaarpit121@gmail.com>
2024-12-07 15:16:38 +0530

    Remove existing project

--- src/components/algolia.js
+++
@@ -1,7 +0,0 @@
-/*import algoliasearch from 'algoliasearch';
-
-const client = algoliasearch('9KR0L2H4CR', '0604eca20b3725e3c9280c24b4952aa9');
-//const algolia = client.initIndex('74f01548dc75832897a9e0cfae75cd5c');
-
-export default client;
-*/
--- src/cridentials.js
+++
@@ -1,2 +0,0 @@
-export const GOOGLE_MAPS_LIBRARIES = ['places'];
-export const REACT_APP_GOOGLE_MAPS_API_KEY = 'AIzaSyCsNh45zRUdhDiJTYblG4vw5gAtWlbTf_4';
