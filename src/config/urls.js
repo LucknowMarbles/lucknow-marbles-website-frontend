cc93ce0fa3b973aa0f96914d92ed9583de690012
Arpit Srivastava <srivastavaarpit121@gmail.com>
2024-12-20 17:47:09 +0530

    Update urls.js

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,11 +1,11 @@
 export const apiUrls = [
     {
         route: "/data/addresses",
-        url: "http://localhost:1337/api/addresses"
+        url: "http://localhost:1337/api/addresses?populate=*"
     },
     {
         route: "/data/contacts",
-        url: "http://localhost:1337/api/contacts"
+        url: "http://localhost:1337/api/contacts?populate=*"
     },
     {
         route: "/data/products",
@@ -13,10 +13,10 @@ export const apiUrls = [
     },
     {
         route: "/data/batches",
-        url: "http://localhost:1337/api/batches"
+        url: "http://localhost:1337/api/batches?populate=*"
     },
     {
         route: "/data/transfers",
-        url: "http://localhost:1337/api/transfers"
+        url: "http://localhost:1337/api/transfers?populate=*"
     }
 ] 
